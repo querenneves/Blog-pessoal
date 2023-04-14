91c00c89f08ca7d27dbe3fbd639e399651404b1a
querenneves <querensantos1@hotmail.com>
2023-04-14 16:54:53 -0300

    Finalizando CRUD

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,14 +3,22 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -22,11 +30,45 @@ public class PostagemController {
 	private PostagemRepository postagemRepository;
 
 	@GetMapping
-	public  ResponseEntity<List<Postagem>> getAll(){
-		
+	public ResponseEntity<List<Postagem>> getAll() {
+
 		return ResponseEntity.ok(postagemRepository.findAll());
-				
-				//Select * from tb_postagens;
+
+		// Select * from tb_postagens;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+		// Select * from tb_postagens;
+	}
+
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		
+	}
+	
+
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+		
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Postagem> delete(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+		
+	}
+
 }
