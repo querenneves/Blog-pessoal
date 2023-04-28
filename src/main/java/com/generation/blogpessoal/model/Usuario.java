56f353a3e4255669ea205128e762af44855045ac
Desenvolvedora full stack Java jr <80592870+querenneves@users.noreply.github.com>
2023-04-28 08:49:58 -0300

    Tirando lombok

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,9 +16,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
